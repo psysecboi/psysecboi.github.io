b095b98394065d9eba6c0c7a7532b239a6aa64f5
Payas <163711414+psysecboi@users.noreply.github.com>
2026-04-20 01:03:23 +0530

    seo verifictation attempt 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import SiteFooter from "@/components/site-footer";
 export const metadata: Metadata = {
   title: "Payas Vaishnav",
   description: "A personal space for systems, projects, and ideas..",
+  verification: {
+    google: "LOxBeV3HrCctugpOGifIpfOTWQHUJf5GF7nxBS45IwA",
+  },
 };
 
 export default function RootLayout({
